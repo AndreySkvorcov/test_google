c66743fdfbda671258b3cc31b8fdd3d84e8fcd81
AndreySkvortsov <askvortsov94@gmail.com>
2017-05-16 17:11:33 +0400

    testgoogle

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,29 +1,37 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
+
+import java.net.URL;
 
 public class Main {
 
     @Test
     public static void testGoogle() throws Exception {
+        String platform_name = "win7";
+        String browser_name = "firefox";
+        String browser_version = "53";
+
+        DesiredCapabilities cap = DesiredCapabilities.firefox();
+        cap.setPlatform(Platform.extractFromSysProperty(platform_name));
+        cap.setBrowserName(browser_name);
+
         System.setProperty("webdriver.gecko.driver", "C://Users//user//Downloads//geckodriver.exe");
-        System.out.println("gecko прошел");
-        WebDriver firefoxDriver = new FirefoxDriver();
-        System.out.println("Драйвер запущен");
+        RemoteWebDriver firefoxDriver = new RemoteWebDriver(new URL("http://localhost:4444/grid/console"), cap);
         firefoxDriver.get("https://www.google.com/");
+        Thread.sleep(5000);
         WebElement element = firefoxDriver.findElement(By.name("q"));
-        System.out.println("Нашли строку поиска");
         element.sendKeys("Симбирсофт");
-        System.out.println("Написали в строке поиска 'Симбирсофт'");
+        Thread.sleep(5000);
         element.submit();
-        System.out.println("нажали submit");
-        WebElement site = firefoxDriver.findElement(By.xpath("//div/h3/a[text()='СимбирСофт']"));
-        System.out.println(site.toString());
-        Assert.assertEquals("СимбирСофт", site.findElement(By.linkText("СимбирСофт")));
+        Thread.sleep(5000);
+//        WebElement site = firefoxDriver.findElement(By.xpath("//div/h3/a/text()"));
+//        System.out.println(site.toString());
+//        Assert.assertEquals("СимбирСофт", site.toString());
 //        System.out.println("На странице отображено: " + firefoxDriver.findElement(By.xpath("//div/h3/a")));
-        firefoxDriver.close();
+        firefoxDriver.quit();
     }
 }
